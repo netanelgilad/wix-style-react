eb5abeea6a40cc65d9c9d4226727fdda2f42b014
TalZa <24567980+TalZa1810@users.noreply.github.com>
2019-12-26 11:13:08 +0200

    adding dataHook constant to navigation buttons

--- src/ModalPreviewLayout/NavigationButton/NavigationButton.js
+++ src/ModalPreviewLayout/NavigationButton/NavigationButton.js
@@ -2,25 +2,29 @@ import React from 'react';
 import IconButton from '../../IconButton';
 import ChevronLeft from 'wix-ui-icons-common/ChevronLeft';
 import ChevronRight from 'wix-ui-icons-common/ChevronRight';
-import { arrowsDirection } from '../constants';
+import { arrowsDirection, dataHooks } from '../constants';
 import styles from '../ModalPreviewLayout.st.css';
 import classNames from 'classnames';
 
-const NavigationButton = ({ direction, onClick }) => (
-  <div className={classNames(styles.navigationButton, styles[direction])}>
-    <IconButton
-      as="button"
-      onClick={onClick}
-      skin="transparent"
-      dataHook={`modal-preview-${direction}`}
-    >
-      {direction === arrowsDirection.rightArrow ? (
-        <ChevronRight />
-      ) : (
-        <ChevronLeft />
-      )}
-    </IconButton>
-  </div>
-);
+const NavigationButton = ({ direction, onClick }) => {
+  const isRightArrow = direction === arrowsDirection.rightArrow;
+
+  return (
+    <div className={classNames(styles.navigationButton, styles[direction])}>
+      <IconButton
+        as="button"
+        onClick={onClick}
+        skin="transparent"
+        dataHook={
+          isRightArrow
+            ? dataHooks.modalPreviewRightArrow
+            : dataHooks.modalPreviewLeftArrow
+        }
+      >
+        {isRightArrow ? <ChevronRight /> : <ChevronLeft />}
+      </IconButton>
+    </div>
+  );
+};
 
 export default NavigationButton;
