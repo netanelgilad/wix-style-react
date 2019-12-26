b28d7848ca86f4768c4d937bffb06d92c137dab2
TalZa <24567980+TalZa1810@users.noreply.github.com>
2019-12-26 18:11:59 +0200

    starting with visual tests for modal preview layout

--- src/ModalPreviewLayout/test/ModalPreviewLayout.visual.js
+++ src/ModalPreviewLayout/test/ModalPreviewLayout.visual.js
@@ -5,38 +5,76 @@ import Button from 'wix-style-react/Button';
 import IconButton from 'wix-style-react/IconButton';
 import Print from 'wix-ui-icons-common/Print';
 import More from 'wix-ui-icons-common/More';
-
 import ascendInvoice from '../../../test/assets/ascend-invoice.jpg';
 import ModalPreviewLayout from '..';
+import Modal from '../../Modal';
+import { modalPreviewLayoutPrivateDriverFactory } from './ModalPreviewLayout.private.uni.driver';
+import { uniTestkitFactoryCreator } from 'wix-ui-test-utils/vanilla';
 
-const commonProps = {};
+const dataHook = 'storybook-modal-preview-layout';
+
+const modalPreviewLayoutUniTestkitFactory = uniTestkitFactoryCreator(
+  modalPreviewLayoutPrivateDriverFactory,
+);
+
+const createDriver = () =>
+  modalPreviewLayoutUniTestkitFactory({
+    wrapper: document.body,
+    dataHook,
+  });
+
+const commonProps = {
+  title: 'Basic Website Design',
+  actions: (
+    <Box align="space-between" width={'276px'}>
+      <Button prefixIcon={<Print />} skin="dark">
+        Print
+      </Button>
+      <Button priority="secondary" skin="light">
+        Send
+      </Button>
+      <IconButton skin="light">
+        <More />
+      </IconButton>
+    </Box>
+  ),
+};
+
+const multipleChildren = ['first', 'second', 'third'].map(ordinalNum => (
+  <Box
+    width="90vw"
+    height="95vh"
+    align="center"
+    verticalAlign="middle"
+    backgroundColor="D80"
+  >
+    {`${ordinalNum} content page`}
+  </Box>
+));
 
 const tests = [
   {
-    describe: 'sanity', // prop name (e.g. size)
+    describe: 'sanity',
     its: [
       {
-        it: 'scrollable', //prop variation (e.g. small)
+        it: 'scrollable',
         props: {
-          title: 'Basic Website Design',
           children: (
             <Box>
               <img src={ascendInvoice} />
             </Box>
           ),
-          actions: (
-            <Box align="space-between" width={'276px'}>
-              <Button prefixIcon={<Print />} skin="dark">
-                Print
-              </Button>
-              <Button priority="secondary" skin="light">
-                Send
-              </Button>
-              <IconButton skin="light">
-                <More />
-              </IconButton>
-            </Box>
-          ),
+        },
+      },
+    ],
+  },
+  {
+    describe: 'Navigation Buttons',
+    its: [
+      {
+        it: 'first child node',
+        props: {
+          children: multipleChildren,
         },
       },
     ],
@@ -46,7 +84,9 @@ const tests = [
 tests.forEach(({ describe, its }) => {
   its.forEach(({ it, props }) => {
     storiesOf(`ModalPreviewLayout/${describe}`, module).add(it, () => (
-      <ModalPreviewLayout {...commonProps} {...props} />
+      <Modal isOpen>
+        <ModalPreviewLayout {...commonProps} {...props} />
+      </Modal>
     ));
   });
 });
